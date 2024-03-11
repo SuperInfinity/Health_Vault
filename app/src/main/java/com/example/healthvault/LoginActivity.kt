62b6b3fc540c9bfe57a6077eaf5f7b4efe5b2259
INFIisjustcool <superinfinity5@gmail.com>
2024-03-12 00:56:28 +0530

    App

--- app/src/main/java/com/example/healthvault/LoginActivity.kt
+++ app/src/main/java/com/example/healthvault/LoginActivity.kt
@@ -1,20 +1,59 @@
 package com.example.healthvault
 
+import android.content.Intent
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.healthvault.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private val binding : ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        // Initialization of FireBase Auth
+        auth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if (password.isEmpty() || email.isEmpty())
+            {
+                Toast.makeText(this, "Enter Credentials..!",  Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful)
+                        {
+                            Toast.makeText(this, "Logged In Sccessfully..!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Failed : ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
+        binding.signupButton.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/healthvault/SignUpActivity.kt
+++ app/src/main/java/com/example/healthvault/SignUpActivity.kt
@@ -2,23 +2,67 @@ package com.example.healthvault
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.healthvault.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 import java.nio.channels.InterruptedByTimeoutException
 
 class SignUpActivity : AppCompatActivity() {
     private val binding : ActivitySignUpBinding by lazy {
         ActivitySignUpBinding.inflate(layoutInflater)
     }
+
+    private lateinit var auth : FirebaseAuth
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.loginSButton.setOnClickListener {
+
+        // Initialization FireBase Auth
+        auth = FirebaseAuth.getInstance()
+
+        binding.registerButton.setOnClickListener {
+            // Get text from edit text field..!
+            val email = binding.email.text.toString()
+            val password = binding.password.text.toString()
+            val rePassword = binding.rePassword.text.toString()
+
+            // Check if any field is blank..?
+            if (email.isEmpty() || password.isEmpty() || rePassword.isEmpty())
+            {
+                Toast.makeText(this, "Please Fill all the Details..!", Toast.LENGTH_SHORT).show()
+            }
+            else if (password != rePassword)
+            {
+                Toast.makeText(this, "Password and Re-Password do not match..!", Toast.LENGTH_SHORT).show()
+            }
+            else if (password.length < 6)
+            {
+                Toast.makeText(this, "Password should be at least 6 charecters..!", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful){
+                            Toast.makeText(this, "Registration Successfull..!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, LoginActivity::class.java))
+                            finish()
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Regsitration Failed : ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
         }
-    //hello this file takes a l;ot to push..!
-        //jajaja
+
     }
 }
